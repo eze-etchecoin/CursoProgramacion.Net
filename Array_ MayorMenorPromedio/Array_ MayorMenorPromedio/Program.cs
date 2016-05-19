82887c2102fd34543963984592a04a663e272d2c
eze-etchecoin <eze.etchecoin.92@gmail.com>
2016-05-18 23:38:38 -0300

    Array Mayor-Menor-Promedio
    Reportar dichos elementos desde un array unidimensional de tamaño 10.

---
+++ Array_ MayorMenorPromedio/Array_ MayorMenorPromedio/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array__MayorMenorPromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] a = new decimal[10];
+            
+            for(int i=0; i<10; i++)
+            {
+                Console.WriteLine("Ingrese el elemento nº" + (i + 1));
+                a[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+
+            decimal mayor = a[0];
+            decimal menor = a[0];
+            decimal suma = a[0];
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(a[i] > mayor)
+                {
+                    mayor = a[i];
+                }
+                if(a[i] < menor)
+                {
+                    menor = a[i];
+                }
+                suma += a[i];
+            }
+
+            decimal promedio = suma / 10;
+
+            Console.WriteLine("\nEl mayor elemento es: " + mayor);
+            Console.WriteLine("El menor elemento es: " + menor);
+            Console.WriteLine("El promedio de los elementos es: " + promedio);
+
+            Console.WriteLine("\nPresione una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
