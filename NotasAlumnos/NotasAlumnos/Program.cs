e09379f2d958267c083b94db4264647fe76520ac
eze-etchecoin <eze.etchecoin.92@gmail.com>
2016-06-04 00:09:11 -0300

    ejercicio notas alumnos

---
+++ NotasAlumnos/NotasAlumnos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotasAlumnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, nota;
+
+            Console.WriteLine("Ingrese la cantidad de alumnos...");
+            n = Convert.ToInt32(Console.ReadLine());
+            Random r = new Random();
+
+            for (int i = 1; i <= n; i++)
+            {
+                nota = r.Next(1, 11);
+                Console.Write("Alumno " + i + "\t" + nota + "\t");
+                for (int j = 0; j < nota; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
